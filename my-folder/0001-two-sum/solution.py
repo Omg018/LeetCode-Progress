cfdda95e87afba613035bd5f59e7e188fb518ef8
Om Gharate <65721507+Omg018@users.noreply.github.com>
2026-08-14 13:15:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.3 MB (58.76%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,48 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-         
-        dic = {}
+        seen = {}
 
         for i, ele in enumerate(nums):
             check = target - ele
-            if check in dic:
-                return [dic[check], i]
-            
-            dic[ele] = i
-
-
-
-
-
-
-
-        # for i in range(len(nums)):
-        #     sol = nums[i] - target
-        #     for j in range(len(nums)):
-        #         if sol == nums[j]:
-                    
-        # for i in range(len(nums)):
-
-        #     for j in range(i+1,len(nums)):
-        #         if (nums[i]+ nums[j] == target):
-        #             arr = []
-        #             arr.append(i)
-        #             arr.append(j)
-        #             return arr
-
-
+            if check in seen:
+                return [seen[check], i]
+            seen[ele] = i        
         
-
-
-
-
-
-
-        # dic = {}
-
-        # for i, num in enumerate(nums):
-        #     comp = target - num
-        #     if comp in dic:
-        #         return [dic[comp], i]
-
-        #     dic[num] = i
