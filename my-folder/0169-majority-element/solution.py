dc9596a8d706a31afa5bdceeed08d910c69cbd76
Om Gharate <65721507+Omg018@users.noreply.github.com>
2026-08-14 13:57:54 +0000

    [LeetCode Sync] Runtime - 11 ms (33.05%), Memory - 21.2 MB (47.36%)

--- my-folder/0169-majority-element/solution.py
+++ my-folder/0169-majority-element/solution.py
@@ -1,13 +1,28 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         seen = {}
-        for i in nums:
-            seen[i] = seen.get(i, 0) + 1
+        seensort = []
+        for num in nums:
+            seen[num] = seen.get(num, 0) + 1
+            # seensort = sorted(seen.keys(), reverse = True)
+        for num, freq in seen.items():
+            if freq > len(nums) // 2:
+                return num
+
+
+
+
+
+
+
+        # seen = {}
+        # for i in nums:
+        #     seen[i] = seen.get(i, 0) + 1
         
-        for key, value in seen.items():
-            if value > len(nums) // 2:
+        # for key, value in seen.items():
+        #     if value > len(nums) // 2:
                
-                return key
+        #         return key
         
 
         
