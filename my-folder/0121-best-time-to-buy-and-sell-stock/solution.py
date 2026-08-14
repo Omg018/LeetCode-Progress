72360d16533f4863f08dad831d8952dd87089241
Om Gharate <65721507+Omg018@users.noreply.github.com>
2026-08-14 13:43:09 +0000

    [LeetCode Sync] Runtime - 63 ms (26.33%), Memory - 28.8 MB (20.97%)

--- my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-folder/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -2,14 +2,31 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = prices[0]
         best = 0
-        best_profit = 0
+        best_price = 0
 
         for price in prices:
             min_price = min(min_price, price)
-            profit = price - min_price
-            best = max(best, profit)
+            best_price =  price - min_price
+            best = max(best, best_price)
 
         return best
+
+
+
+
+
+
+        # min_price = prices[0]
+        # best = 0
+        # best_profit = 0
+
+        # for price in prices:
+        #     min_price = min(min_price, price)
+        #     profit = price - min_price
+        #     best = max(best, profit)
+
+        # return best
+
         # sortedprices = sorted(prices)
         # tobuyin = sortedprices[0]
         # best = 0
